488fdc71d69c60833b3b148fd144e70f62188926
adavid9 <dawid.antecki7@gmail.com>
2019-06-10 16:26:59 +0200

    Enabled MVC for "watchers"
    Added new feature that allows user to enter the
    web page using "localhost:8000". Previously there
    was no possibility to do that. Configured for now the
    security to allows all users to enter the welcome page.
    
    TODO:
    Improve the SecurityConfiguration

---
+++ src/main/java/com/dawidantecki/watchers/configuration/WebMvcConfiguration.java
@@ -0,0 +1,37 @@
+package com.dawidantecki.watchers.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:locale/messages");
+
+        return messageSource;
+    }
+}
