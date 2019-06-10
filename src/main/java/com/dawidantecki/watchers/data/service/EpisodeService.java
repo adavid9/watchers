4f55ea28ed53e3c67f680e6a1638144ef42e38fb
adavid9 <dawid.antecki7@gmail.com>
2019-06-10 13:20:38 +0200

    Prepared episode service test
    Added a new method to episode service that allows
    to add a collection of episodes, also wrote tests
    for that service.

--- src/main/java/com/dawidantecki/watchers/data/service/EpisodeService.java
+++ src/main/java/com/dawidantecki/watchers/data/service/EpisodeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -30,6 +31,14 @@ public class EpisodeService {
     public void addEpisode(Episode episode) {
         if (episode != null)
             episodeRepository.save(episode);
+    }
+
+    public void addEpisodes(Collection<Episode> episodes) {
+        if (episodes.size() > 0)
+            episodes.forEach(x -> {
+                if (x != null)
+                    addEpisode(x);
+            });
     }
 
     public void deleteEpisodeById(long id) {
---
+++ src/test/java/com/dawidantecki/watchers/data/service/EpisodeServiceTest.groovy
@@ -0,0 +1,47 @@
+package com.dawidantecki.watchers.data.service
+
+import com.dawidantecki.watchers.data.entity.Episode
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class EpisodeServiceTest extends Specification {
+
+    @Autowired
+    EpisodeService episodeService
+
+    def "should add episode to the database"() {
+        when:
+        Episode episode = new Episode("title", "description")
+        and:
+        episodeService.addEpisode(episode)
+        then:
+        episode == episodeService.findById(episode.id)
+    }
+
+    def "should delete episode if previously added to the database"() {
+        when:
+        Episode episode = new Episode("title", "description")
+        and:
+        episodeService.addEpisode(episode)
+        then:
+        episodeService.findById(episode.id)
+        when:
+        episodeService.deleteEpisodeById(episode.id)
+        then:
+        !episodeService.findById(episode.id)
+    }
+
+    def "should return valid size of episodes added to the database"() {
+        given:
+        List<Episode> episodes = []
+        when:
+        episodes.add(new Episode("title1", "description1"))
+        episodes.add(new Episode("title2", "description2"))
+        and:
+        episodeService.addEpisodes(episodes)
+        then:
+        episodeService.findAll().size() == episodes.size()
+    }
+}
